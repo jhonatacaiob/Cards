63c7be4173e328eb2f014be67f2764efc1a3133e
unknown <jhonatacaiob@gmail.com>
2021-11-17 13:30:03 -0300

    Terminada a sessão 3

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -2,7 +2,19 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "greets the world" do
-    assert Cards.hello() == :world
+  test "Create_deck makes 20 cards" do
+    assert length(Cards.create_deck) == 20
+  end
+
+  test "Create hand equal hand_size" do
+    hand_size = 3
+    {hand, _deck} = Cards.create_hand(hand_size)
+    assert length(hand) == hand_size
+  end
+
+
+  test "shuffling a deck ramdomizes it" do
+    deck = Cards.create_deck()
+    refute deck == Cards.shuffle(deck)
   end
 end
